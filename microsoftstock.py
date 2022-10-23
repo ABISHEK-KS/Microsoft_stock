388115d63d2649c403165dc678271273e02c6bff
ABISHEK-KS <97246536+ABISHEK-KS@users.noreply.github.com>
2022-10-23 23:05:10 +0530

    Add files via upload

---
+++ microsoftstock.py
@@ -0,0 +1,12 @@
+import pandas as pd 
+import sys
+import os
+import pandas_profiling as pp
+import numpy as np 
+np.set_printoptions(threshold=sys.maxsize)
+
+df=pd.read_csv("C:/Users/ksabi/OneDrive/Desktop/upload to github/Microsoft stock/microsoft_stocks.csv","r")
+prof=pp.ProfileReport(df,title='MICROSOFT STOCK DATA PROFILING')
+pd.set_option('display.max_colwidth', None)
+prof.to_file("Microsoft_Stock_Data_Profiling.json")
+prof.to_file("Microsoft_Stock_Data_Profiling.html")
